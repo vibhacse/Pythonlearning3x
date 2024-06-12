47697244ed80e9f6383556992207a160e49a07ad
vibhacse <vibhacse@gmail.com>
2024-06-12 08:33:20 +0530

    practice code

---
+++ 12/6/practice.py
@@ -0,0 +1,32 @@
+age = int(input("Enter your age:"))
+if age > 18:
+    print("You are eligible to Vote")
+else:
+    print("Not eligible to Vote")
+
+a = 10
+b = -1
+c = 34
+
+if a > b and a > c:
+    print("A is greater")
+elif b > a and b > c:
+    print("B is greater")
+else:
+    print("C is greater")
+
+i = 1
+while i <= 5:
+    print(i)
+    i = i+1
+
+
+for i in range(5):
+    print(i)
+
+for i in range(2,5):
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
+
